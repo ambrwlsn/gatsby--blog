ee3173e3947013a7d9a22a7c798ae270617f9ce9
ambrwlsn <arw89@uclive.ac.nz>
2018-12-27 14:19:42 +0100

    Added correct pages that mirror my current site

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,54 +5,27 @@ import Bio from '../components/bio'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
-const BlogIndex = props => {
-  console.log(props)
+const Index = props => {
   const siteTitle = props.data.site.siteMetadata.title
-  const posts = props.data.allMarkdownRemark.edges
 
   return (
     <Layout location={props.location} title={siteTitle}>
       <SEO title="All posts" keywords={['blog', 'gatsby', 'javascript', 'react']} />
+      <p>
+        See my blog posts <Link to="/blog/">here</Link>.
+      </p>
       <Bio />
-      {posts.map(({ node }) => {
-        const title = node.frontmatter.title || node.fields.slug
-        return (
-          <div key={node.fields.slug}>
-            <h3>
-              <Link style={{ boxShadow: 'none' }} to={node.fields.slug}>
-                {title}
-              </Link>
-            </h3>
-            <small>{node.frontmatter.date}</small>
-            <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-          </div>
-        )
-      })}
     </Layout>
   )
 }
 
-export default BlogIndex
+export default Index
 
 export const pageQuery = graphql`
   query {
     site {
       siteMetadata {
         title
-      }
-    }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            title
-          }
-        }
       }
     }
   }
