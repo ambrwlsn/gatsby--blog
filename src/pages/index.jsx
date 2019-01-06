244597340c3f2661474a8a5ecce22068a8e4262d
ambrwlsn <arw89@uclive.ac.nz>
2019-01-06 13:38:53 +0100

    Added social media icons to homepage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,6 +4,9 @@ import { Link, graphql } from 'gatsby'
 import Bio from '../components/bio'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import Twitter from './img/twitter.svg'
+import Github from './img/github.svg'
+import Linkedin from './img/linkedin.svg'
 
 const Index = props => {
   const siteTitle = props.data.site.siteMetadata.title
@@ -14,6 +17,10 @@ const Index = props => {
       <p>
         See my blog posts <Link to="/blog/">here</Link>.
       </p>
+      <img src={Twitter} alt="twitter" />
+      <img src={Github} alt="github" />
+      <img src={Linkedin} alt="linkedin" />
+
       <Bio />
     </Layout>
   )
