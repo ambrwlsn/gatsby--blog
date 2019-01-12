6f15c8358e9a904a159d540932c818e9d0e605ba
ambrwlsn <arw89@uclive.ac.nz>
2019-01-12 23:37:31 +0100

    Formatting on save changes to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,62 +34,65 @@ module.exports = {
     {
       resolve: `gatsby-transformer-remark`,
       options: {
-        plugins: [{
-          resolve: `gatsby-remark-social-cards`,
-          options: {
-            title: {
-              // This is the frontmatter field the title should come from
-              field: 'title',
-              // Currently only supports DejaVuSansCondensed
-              // More fonts coming soon!
-              font: 'DejaVuSansCondensed',
-              color: 'black', // black|white
-              size: 48, // 16|24|32|48|64
-              style: 'bold', // normal|bold|italic
-              x: null, // Will default to xMargin
-              y: null, // Will default to yMargin
+        plugins: [
+          {
+            resolve: `gatsby-remark-social-cards`,
+            options: {
+              title: {
+                // This is the frontmatter field the title should come from
+                field: 'title',
+                // Currently only supports DejaVuSansCondensed
+                // More fonts coming soon!
+                font: 'DejaVuSansCondensed',
+                color: 'black', // black|white
+                size: 48, // 16|24|32|48|64
+                style: 'bold', // normal|bold|italic
+                x: null, // Will default to xMargin
+                y: null, // Will default to yMargin
+              },
+              meta: {
+                // The parts array is what generates the bottom text
+                // Pass an array with strings and objects
+                // The following array will generate:
+                // "- Author Name » September 13"
+                // The objects are used to pull data from your markdown's
+                // frontmatter. { field: "author" } pulls the author set
+                // in the frontmatter. { field: "category" } would pull
+                // the category set. Any field can be used as parts
+                // Note: Only pass the "format" property on date fields
+                parts: [
+                  '- ',
+                  {
+                    field: 'author',
+                  },
+                  ' » ',
+                  {
+                    field: 'date',
+                    format: 'mmmm dS',
+                  },
+                ],
+                // Currently only supports DejaVuSansCondensed
+                // More fonts coming soon!
+                font: 'Verdana',
+                color: 'black', // black|white
+                size: 24, // 16|24|32|48|64
+                style: 'normal', // normal|bold|italic
+                x: null, // Will default to xMargin
+                y: null, // Will default to cardHeight - yMargin - size
+              },
+              background: '#FFFFFF', // Background color for the card
+              xMargin: 24, // Edge margin used when x value is not set
+              yMargin: 24, // Edge margin used when y value is not set
             },
-            meta: {
-              // The parts array is what generates the bottom text
-              // Pass an array with strings and objects
-              // The following array will generate:
-              // "- Author Name » September 13"
-              // The objects are used to pull data from your markdown's
-              // frontmatter. { field: "author" } pulls the author set
-              // in the frontmatter. { field: "category" } would pull
-              // the category set. Any field can be used as parts
-              // Note: Only pass the "format" property on date fields
-              parts: [
-                "- ",
-                {
-                  field: "author"
-                },
-                " » ",
-                {
-                  field: "date",
-                  format: "mmmm dS"
-                },
-              ],
-              // Currently only supports DejaVuSansCondensed
-              // More fonts coming soon!
-              font: "Verdana",
-              color: "black", // black|white
-              size: 24, // 16|24|32|48|64
-              style: "normal", // normal|bold|italic
-              x: null, // Will default to xMargin
-              y: null, // Will default to cardHeight - yMargin - size
-            },
-            background: "#FFFFFF", // Background color for the card
-            xMargin: 24, // Edge margin used when x value is not set
-            yMargin: 24, // Edge margin used when y value is not set
-          }
-        }],
+          },
+        ],
       },
     },
     {
       resolve: `gatsby-transformer-remark`,
       options: {
-        plugins: [{
+        plugins: [
+          {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 400,
@@ -122,7 +125,7 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Amber's Blog`,
-        short_name: `A`,
+        short_name: `A's blog`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
