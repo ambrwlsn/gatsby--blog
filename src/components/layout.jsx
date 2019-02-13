7141b91d80dbbac3a3078d4c2cecf4463b3b85b3
ambrwlsn <arw89@uclive.ac.nz>
2019-02-13 20:13:57 +0100

    Add SVG logo to layout

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,36 +1,21 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import styled from 'styled-components'
+import A from '../components/logo'
 
 const Wrapper = styled.div`
   margin: 2em;
 `
-const Logo = styled.div`
-  font-family: 'Courgette', cursive;
-  font-size: 2em;
-  box-shadow: none;
-  color: #000;
-`
+
 const Layout = ({ children }) => {
   // eslint-disable-next-line
   const rootPath = `${__PATH_PREFIX__}/`
 
   return (
     <Wrapper>
-      <h1
-        style={{
-          marginTop: 0,
-        }}
-      >
-        <Link
-          style={{
-            textDecoration: 'none',
-          }}
-          to="/"
-        >
-          <Logo>a</Logo>
-        </Link>
-      </h1>
+      <Link to="/">
+        <A width={50} height={50} color="#000" />
+      </Link>
       {children}
     </Wrapper>
   )
