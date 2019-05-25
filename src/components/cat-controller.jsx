1a62478631b5f78bb3cc295221f5c109baaba16e
DIonysos Dajka <diondiondion@gmail.com>
2019-05-25 21:45:27 +0200

    Move throttle function to separate file

--- src/components/cat-controller.jsx
+++ src/components/cat-controller.jsx
@@ -2,43 +2,7 @@ import React, { useState, useRef, useEffect } from 'react'
 import { watchViewport, unwatchViewport } from 'tornis'
 
 import Kitty from '@components/img/cat'
-
-// Returns a function, that, when invoked, will only be triggered at most once
-// during a given window of time. Normally, the throttled function will run
-// as much as it can, without ever going more than once per `wait` duration;
-// but if you'd like to disable the execution on the leading edge, pass
-// `{leading: false}`. To disable execution on the trailing edge, ditto.
-function throttle(func, wait, options) {
-  var context, args, result
-  var timeout = null
-  var previous = 0
-  if (!options) options = {}
-  var later = function() {
-    previous = options.leading === false ? 0 : Date.now()
-    timeout = null
-    result = func.apply(context, args)
-    if (!timeout) context = args = null
-  }
-  return function() {
-    var now = Date.now()
-    if (!previous && options.leading === false) previous = now
-    var remaining = wait - (now - previous)
-    context = this
-    args = arguments
-    if (remaining <= 0 || remaining > wait) {
-      if (timeout) {
-        clearTimeout(timeout)
-        timeout = null
-      }
-      previous = now
-      result = func.apply(context, args)
-      if (!timeout) context = args = null
-    } else if (!timeout && options.trailing !== false) {
-      timeout = setTimeout(later, remaining)
-    }
-    return result
-  }
-}
+import throttle from '@utils/throttle'
 
 function CatController({ className }) {
   // State for storing mouse coordinates
@@ -51,7 +15,7 @@ function CatController({ className }) {
       if (mouse.changed) {
         setCoords([mouse.x, mouse.y])
       }
-    }, 350)
+    }, 50)
     watchViewport(updateValues)
 
     return () => {
@@ -75,8 +39,6 @@ function CatController({ className }) {
     clampedEyeX = Math.max(-7.5, Math.min(7.5, x - eyeX))
     clampedEyeY = Math.max(-7.5, Math.min(7.5, y - eyeY))
   }
-
-  console.log('rendering!')
 
   return (
     <Kitty
