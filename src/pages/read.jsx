260592c97a7d9ac56cd638abd964b192d79e922f
Amber Wilson <amber.wilson@n26.com>
2019-07-12 10:06:14 +0200

    Add new book

--- src/pages/read.jsx
+++ src/pages/read.jsx
@@ -12,12 +12,13 @@ import myThoughtsExactly from '@pages/img/read/my_thoughts_exactly.jpg'
 import ourHouse from '@pages/img/read/our_house.jpg'
 import whenBreathBecomesAir from '@pages/img/read/when_breath_becomes_air.jpg'
 import theDollsAlphabet from '@pages/img/read/the_dolls_alphabet.jpg'
-import theBear from '@pages/img/read/the_bear.jpg'
+// import theBear from '@pages/img/read/the_bear.jpg'
 import theHurt from '@pages/img/read/this_is_going_to_hurt.jpg'
+import angryPlanet from '@pages/img/read/angry-planet.jpg'
 
 import Headphones from '@pages/img/read/headphones'
 import Kindle from '@pages/img/read/kindle'
-import Book from '@pages/img/read/book'
+// import Book from '@pages/img/read/book'
 
 const ReviewSection = styled.article`
   display: flex;
@@ -198,7 +199,7 @@ const Reading = () => (
         typeColour="#7c8ebc"
       />
     </ReviewSection>
-    <ReviewSection>
+    {/* <ReviewSection>
       <BookReview
         title="The Bear Went Over The Mountain by William Kotzwinkle"
         review="Reading in progress - 50 pages to go."
@@ -221,7 +222,7 @@ const Reading = () => (
         methodColour="#bbcae3"
         typeColour="#8e97b4"
       />
-    </ReviewSection>
+    </ReviewSection> */}
     <ReviewSection>
       <BookReview
         title="This Is Going to Hurt: Secret Diaries of a Junior Doctor by Adam Kay"
@@ -244,6 +245,25 @@ const Reading = () => (
         lengthColour="#F6FB5B"
         methodColour="#32B2DF"
         typeColour="#B894A1"
+      />
+    </ReviewSection>
+    <ReviewSection>
+      <BookReview
+        title="The Long Way to a Small, Angry Planet by Becky Chambers"
+        review="Oh I loved this one - the characters, the dialogue, and the imagery. I found myself feeling multiple emotions throughout - from anger to sadness to fear. I don't even generally like science fiction books but this one is so down-to-earth (not literally), and so full of feeling and beautiful ideas that I never wanted to put it down. This book made me examine myself, and humanity. It put into perspective that while humans can be great, they can also be flawed. You should read it to properly understand what I mean ;) Also, the different alien characters in this book are simply delightful. We get to hear about their histories, their peculiarities, their cultures. They are all so endearing and the author makes them seem so plausible and real! I found this really incredible. Another thing I love is that there are gay and non-binary characters - I think it's important for mainstream books to have more of these. The feelings and beautiful perspectives I got from this book will stay with me for a long time - I feel lucky to have found this one (through a friend's recommendation) :)"
+        picture={angryPlanet}
+        picAlt="The Long Way to a Small, Angry Planet book cover"
+        methodAlt="Kindle"
+        method={
+          <Kindle width={110} height={110} fill="#d6dae4" stroke="#d6dae4" />
+        }
+        length="432 pages"
+        type="Enlightening, funny, surprising."
+        reviewColour="#E8D4BC"
+        buttonColour="#C7AD8E"
+        lengthColour="#4E76AF"
+        methodColour="#2C2D28"
+        typeColour="#B7BDD0"
       />
     </ReviewSection>
   </Layout>
