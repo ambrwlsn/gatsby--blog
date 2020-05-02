06dbe69f67c9a01e2ae10fa9327a856ae86c8a4e
ambrwlsn <arw89@uclive.ac.nz>
2020-05-02 19:41:26 +0200

    Update layout component

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,56 +2,102 @@ import React, { useContext, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
 import styled from 'styled-components'
-import A from '@components/img/logo'
+import Logo from '@components/img/logo'
 import ThemeContext from '@context/theme-context'
 import ModeToggle from '@components/mode-toggle'
-import Nav from '@components/navigation'
 
 const Wrapper = styled.div`
   max-width: 100%;
-  padding: 2rem;
+  height: 40px;
   display: flex;
   justify-content: space-around;
-  @media (max-width: 800px) {
-    padding: 1rem;
+  align-items: flex-start;
+  padding: 1rem 1rem 0 1rem;
+  @media (min-width: 800px) {
+    padding: 2rem 2rem 3rem 2rem;
   }
 `
 
-const sizes = {
+const NavigationList = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  font-size: 1.3rem;
+  font-family: 'Courgette';
+
+  @media (min-width: 800px) {
+    font-size: 2.6rem;
+  }
+`
+
+const NavigationListItem = styled.li`
+  margin-right: 1.8rem;
+`
+
+const NavigationLink = styled(Link)`
+  text-decoration: none;
+`
+
+const logoSizes = {
   desktop: 50,
   mobile: 22,
 }
 
-const Logo = styled(A)`
-  width: ${sizes.desktop}px;
-  height: ${sizes.desktop}px;
+const SiteLogo = styled(Logo)`
+  width: ${logoSizes.desktop}px;
+  height: ${logoSizes.desktop}px;
+  padding-top: 0.2rem;
+  vertical-align: top;
 
   @media (max-width: 800px) {
-    width: ${sizes.mobile}px;
-    height: ${sizes.mobile}px;
+    width: ${logoSizes.mobile}px;
+    height: ${logoSizes.mobile}px;
   }
 `
 
-const LovelyLink = styled(Link)`
+const SiteLogoLink = styled(Link)`
   color: inherit;
   display: inline;
 `
 
-const Layout = ({ children }) => {
+const Header = () => {
   const context = useContext(ThemeContext)
 
   return (
+    <Wrapper>
+      <SiteLogoLink to="/">
+        <SiteLogo width={50} height={50} color="currentColor" />
+      </SiteLogoLink>
+
+      <nav>
+        <NavigationList>
+          <NavigationListItem>
+            <NavigationLink to="/blog">Blog</NavigationLink>
+          </NavigationListItem>
+
+          <NavigationListItem>
+            <NavigationLink to="/learn">Learn</NavigationLink>
+          </NavigationListItem>
+
+          <NavigationListItem>
+            <NavigationLink to="/read">Read</NavigationLink>
+          </NavigationListItem>
+        </NavigationList>
+      </nav>
+
+      <ModeToggle
+        checked={!context.isDarkTheme}
+        onChange={context.toggleDark}
+      />
+    </Wrapper>
+  )
+}
+
+const Layout = ({ children }) => {
+  return (
     <Fragment>
-      <Wrapper>
-        <LovelyLink to="/">
-          <Logo width={50} height={50} color="currentColor" />
-        </LovelyLink>
-        <Nav />
-        <ModeToggle
-          checked={!context.isDarkTheme}
-          onChange={context.toggleDark}
-        />
-      </Wrapper>
+      <Header />
       {children}
     </Fragment>
   )
