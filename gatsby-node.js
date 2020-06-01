b2f20ef9f12e6e4ef5b80cab1c803aced8e32778
ambrwlsn <arw89@uclive.ac.nz>
2020-06-01 20:05:17 +0200

    Create plant pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,11 +4,12 @@ const { createFilePath } = require('gatsby-source-filesystem')
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
-  const blogPost = path.resolve('./src/templates/blog-post.jsx')
+  const blogPostTemplate = path.resolve('./src/templates/blog-post.jsx')
+  const plantPostTemplate = path.resolve('./src/templates/plant-post.jsx')
   const tagTemplate = path.resolve('src/templates/tags.jsx')
 
-  // Get a full list of markdown posts
-  const markdownQueryResult = await graphql(`
+  // Get a full list of blog markdown posts
+  const markdownQueryBlogResult = await graphql(`
     {
       allMarkdownRemark(
         sort: { fields: [frontmatter___date], order: DESC }
@@ -30,23 +31,71 @@ exports.createPages = async ({ graphql, actions }) => {
     }
   `)
 
-  if (markdownQueryResult.errors) {
-    console.error(markdownQueryResult.errors)
-    throw markdownQueryResult.errors
+  if (markdownQueryBlogResult.errors) {
+    console.error(markdownQueryBlogResult.errors)
+    throw markdownQueryBlogResult.errors
   }
 
   // Create blog posts pages.
-  const posts = markdownQueryResult.data.allMarkdownRemark.edges
+  const blogPosts = markdownQueryBlogResult.data.allMarkdownRemark.edges
 
-  posts.forEach((post, index) => {
-    const previous = index === posts.length - 1 ? null : posts[index + 1].node
-    const next = index === 0 ? null : posts[index - 1].node
+  blogPosts.forEach((blogPost, index) => {
+    const previous =
+      index === blogPosts.length - 1 ? null : blogPosts[index + 1].node
+    const next = index === 0 ? null : blogPosts[index - 1].node
 
     createPage({
-      path: `blog${post.node.fields.slug}`,
-      component: blogPost,
+      path: `blog${blogPost.node.fields.slug}`,
+      component: blogPostTemplate,
       context: {
-        slug: post.node.fields.slug,
+        slug: blogPost.node.fields.slug,
+        previous,
+        next,
+      },
+    })
+  })
+
+  // Get a full list of plant markdown posts
+  const markdownQueryPlantResult = await graphql(`
+    {
+      allMarkdownRemark(
+        filter: { fileAbsolutePath: { regex: "/content/plants/" } }
+      ) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              subject
+              posttype
+              number
+              updated
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  if (markdownQueryPlantResult.errors) {
+    console.error(markdownQueryPlantResult.errors)
+    throw markdownQueryPlantResult.errors
+  }
+
+  // Create plant posts pages.
+  const plantPosts = markdownQueryPlantResult.data.allMarkdownRemark.edges
+
+  plantPosts.forEach((plantPost, index) => {
+    const previous =
+      index === plantPosts.length - 1 ? null : plantPosts[index + 1].node
+    const next = index === 0 ? null : plantPosts[index - 1].node
+
+    createPage({
+      path: `plants${plantPost.node.fields.slug}`,
+      component: plantPostTemplate,
+      context: {
+        slug: plantPost.node.fields.slug,
         previous,
         next,
       },
@@ -56,7 +105,7 @@ exports.createPages = async ({ graphql, actions }) => {
   // Tag pages:
   let tags = []
   // Iterate through each post, putting all found tags into `tags`
-  _.each(posts, edge => {
+  _.each(blogPosts, edge => {
     if (_.get(edge, 'node.frontmatter.tags')) {
       edge.node.frontmatter.tags.forEach(tag => {
         tags = tags.concat(tag)
@@ -77,15 +126,14 @@ exports.createPages = async ({ graphql, actions }) => {
   })
 }
 
-exports.onCreateNode = ({ node, actions, getNode }) => {
+exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
-
   if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
     createNodeField({
-      name: `slug`,
       node,
-      value,
+      name: `slug`,
+      value: slug,
     })
   }
 }
