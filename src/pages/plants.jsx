46c283df6c0ae770eda297dbe38aeff2ec14f284
ambrwlsn <arw89@uclive.ac.nz>
2020-06-01 20:06:54 +0200

    Add plant page and plant post template

---
+++ src/pages/plants.jsx
@@ -0,0 +1,100 @@
+import React from 'react'
+import { graphql, Link } from 'gatsby'
+import styled from 'styled-components'
+
+import Layout from '@components/layout'
+import SEO from '@components/seo'
+
+const PhotoGrid = styled.div`
+  display: grid;
+  margin: 0 auto;
+  width: 90%;
+  grid-template-columns: 50% 50%;
+  grid-template-rows: repeat(2, auto);
+
+  @media (min-width: 800px) {
+    width: 55%;
+    margin-bottom: 1rem;
+  }
+`
+
+const Title = styled.h1`
+  text-align: center;
+  font-family: 'Courgette', sans-serif;
+`
+
+const Intro = styled.p`
+  margin: 0 auto;
+  line-height: 1.7;
+  margin-bottom: 2rem;
+  width: 55%;
+  @media (max-width: 800px) {
+    margin-bottom: 1rem;
+    width: 90%;
+  }
+`
+
+const IntroStyles = styled.span`
+  font-size: 1rem;
+`
+
+const Plants = props => {
+  const plantPosts = props.data.allMarkdownRemark.edges
+
+  return (
+    <Layout>
+      <SEO title="Plants I Own" keywords={['plants', 'plant collection']} />
+      <Title>Plants</Title>
+      <IntroStyles>
+        <Intro>
+          Here are the plants I've collected in my flat - there is care info,
+          pictures, progress statuses and more! This page is in progress{' '}
+          <span role="img" aria-label="seedling emoji">
+            &#x1f331;
+          </span>
+        </Intro>
+        <PhotoGrid>
+          {plantPosts.map(({ node }) => {
+            const { subject, number } = node.frontmatter
+            return (
+              <React.Fragment key={number}>
+                <Link to={`/plants/${node.fields.slug}`}>{subject}</Link>
+              </React.Fragment>
+            )
+          })}
+        </PhotoGrid>
+      </IntroStyles>
+    </Layout>
+  )
+}
+
+export default Plants
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        siteUrl
+      }
+    }
+    allMarkdownRemark(
+      filter: { fileAbsolutePath: { regex: "/content/plants/" } }
+    ) {
+      edges {
+        node {
+          html
+          fields {
+            slug
+          }
+          frontmatter {
+            subject
+            posttype
+            number
+            updated
+          }
+        }
+      }
+    }
+  }
+`
---
+++ src/templates/plant-post.jsx
@@ -0,0 +1,112 @@
+import React from 'react'
+import { Link, graphql } from 'gatsby'
+import ContentWrapper from '@components/content-wrapper'
+import styled from 'styled-components'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const PlantNavigation = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  list-style: none;
+  padding: 0;
+`
+
+const Title = styled.h1`
+  font-family: 'Courgette', cursive;
+  font-size: 2.3rem;
+  @media (max-width: 800px) {
+    margin: 0;
+    margin-bottom: 0.8em;
+  }
+`
+
+const Content = styled.div`
+  line-height: 1.7;
+  font-size: 1rem;
+`
+
+const PlantPostTemplate = props => {
+  const plantPost = props.data.markdownRemark
+  const { previous, next } = props.pageContext
+
+  return (
+    <Layout>
+      <ContentWrapper>
+        <SEO
+          title={plantPost.frontmatter.subject}
+          description=""
+          keywords=""
+          slug={plantPost.fields.slug}
+        />
+        <main>
+          <Title className="p-name">{plantPost.frontmatter.subject}</Title>
+          <Content dangerouslySetInnerHTML={{ __html: plantPost.html }} />
+        </main>
+        <hr />
+        <PlantNavigation>
+          <li>
+            {previous && (
+              <Link
+                to={`/plants${previous.fields.slug}`}
+                rel="prev"
+                style={{
+                  textDecoration: 'none',
+                  fontWeight: '900',
+                }}
+              >
+                ← previous
+              </Link>
+            )}
+          </li>
+          <li>
+            {next && (
+              <Link
+                to={`/plants${next.fields.slug}`}
+                rel="next"
+                style={{
+                  textDecoration: 'none',
+                  fontWeight: '900',
+                }}
+              >
+                next →
+              </Link>
+            )}
+          </li>
+        </PlantNavigation>
+      </ContentWrapper>
+    </Layout>
+  )
+}
+
+export default PlantPostTemplate
+
+/* https://github.com/gatsbyjs/gatsby/tree/master/packages/
+ * gatsby-transformer-remark
+ *
+ * Each Markdown file is parsed into a node of type MarkdownRemark.
+ */
+
+export const pageQuery = graphql`
+  query PlantPostBySlug($slug: String!) {
+    site {
+      siteMetadata {
+        title
+        author
+        siteUrl
+      }
+    }
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      id
+      html
+      fields {
+        slug
+      }
+      frontmatter {
+        subject
+      }
+    }
+  }
+`
