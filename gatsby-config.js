b39c88e071a75b0e7e2d9727a6d24ea260ffa22a
ambrwlsn <arw89@uclive.ac.nz>
2020-06-01 20:04:31 +0200

    Add plant pages to file system

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,6 +41,13 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        path: `${__dirname}/content/plants`,
+        name: 'plants',
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         path: `${__dirname}/content/blog`,
         name: 'blog',
       },
