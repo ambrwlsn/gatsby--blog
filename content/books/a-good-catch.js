7c12413b23d8b27072390721dde9f76fa5c1e7ad
ambrwlsn <arw89@uclive.ac.nz>
2020-08-19 20:24:17 +0200

    Add book review

--- content/books/a-good-catch.js
+++ content/books/a-good-catch.js
@@ -7,7 +7,7 @@ const AGoodCatch = () => {
   return (
     <BookReview
       title="A Good Catch by Fern Britton"
-      review="50% through"
+      review="I bought this because I quite fancied reading a book that was given an award for being a good story to read while on holiday. However, on the whole I was disappointed with a number of aspects in the book. I felt most characters are good by themselves, with lots of potential. But I think they didn't go well together. The relationship between them doesn't seem realistic at all. The storyline seemed a bit chaotic as well, and no part of it seemed to be given time to develop in a more meaningful way. I feel like the author threw together lots of parts of the story just to see if they'd stick. The book is an easy read though with some twists and turns that aren't too predictable, and some of the dialogue is quite enjoyable."
       picture={goodCatch}
       picAlt="A Good Catch book cover"
       methodAlt="Kindle"
@@ -20,7 +20,7 @@ const AGoodCatch = () => {
         />
       }
       length="434 pages"
-      type=""
+      type="Incohesive, under-developed, emotional"
       reviewColour="#fcd4d8"
       buttonColour="#c3a3a7"
       lengthColour="#f5e6be"
