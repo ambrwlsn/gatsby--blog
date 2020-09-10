9800d8526d9d9209cff21ebcaf41ec8b627c7f0a
ambrwlsn <arw89@uclive.ac.nz>
2020-09-10 10:51:32 +0200

    Add finished book review

--- content/books/everything-inside.js
+++ content/books/everything-inside.js
@@ -7,13 +7,13 @@ const EverythingInside = () => {
   return (
     <BookReview
       title="Everything Inside by Edwidge Danticat"
-      review="80% through"
+      review="Each story in this book is different although most are focused around either Miami, Florida, Haiti, or both. The stories focus subtly on relatable but difficult topics. The topics seem to be ones that nobody talks about much, but rather tend to stay within families. There is something incredibly human about each story. I feel like some stories moved quite slowly, where many actions and reactions are described in detail. Perhaps some readers would like the point of each story to arrive more quickly. But, I found something calming about being brought through each story day-by-day rather than all at once. This aspect added to the human feeling of each story for me. The stories are must less 'here is an action movie of someone's life' and rather 'here is this person's real life, boring parts and all'. This is something I really appreciate, because I feel like so few authors (and so few people) tell such relatable stories that validate you and make you feel like you are a normal, okay human after all."
       picture={everythingInside}
       picAlt="Everything Inside book cover"
       methodAlt="Kindle"
       method={<Book width={110} height={110} fill="#d6dae4" stroke="#d6dae4" />}
       length="220 pages"
-      type=""
+      type="Insightful, human, relatable."
       reviewColour="#EAEAF0"
       buttonColour="#B2B3B9"
       lengthColour="#77C77F"
